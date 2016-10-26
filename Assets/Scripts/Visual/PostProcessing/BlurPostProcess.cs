b66d1190ac5830240839a57ef1367339dbce0a21
satirikasha <satirikasha@gmail.com>
2016-10-27 01:04:26 +0300

    Furter blur optimisations

--- Assets/Scripts/Visual/PostProcessing/BlurPostProcess.cs
+++ Assets/Scripts/Visual/PostProcessing/BlurPostProcess.cs
@@ -12,7 +12,10 @@ namespace UnityStandardAssets.ImageEffects {
         [Range(0, 1)]
         public float Intensity;
 
-        private const int MaxRadius = 32;
+        private const int MaxRadius = 30;
+        private const float MaxDownsample = 0.15f;
+        // Debug: Remove downsampling
+        //private const float MaxDownsample = 1f;
 
         private Dictionary<int, float[]> _KernelCache = new Dictionary<int, float[]>();
 
@@ -28,12 +31,23 @@ namespace UnityStandardAssets.ImageEffects {
 
         void OnRenderImage(RenderTexture source, RenderTexture destination) {
             var radius = (int)(MaxRadius * Intensity);
-            var rt = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            if (radius == 0) {
+                Graphics.Blit(source, destination);
+                return;
+            }
+            var downsample = MaxDownsample + (1 - MaxDownsample) * (1 - Intensity) * (1 - Intensity);
+            //var rt = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            var rt1 = RenderTexture.GetTemporary((int)(source.width * downsample), (int)(source.height * downsample), 0, source.format);
+            var rt2 = RenderTexture.GetTemporary((int)(source.width * downsample), (int)(source.height * downsample), 0, source.format);
             material.SetInt("_Radius", radius);
             material.SetFloatArray("_Kernel", GetKernel(radius));
-            Graphics.Blit(source, rt, material, 0);
-            Graphics.Blit(rt, destination, material, 1);
-            RenderTexture.ReleaseTemporary(rt);
+            Graphics.Blit(source, rt1);
+            Graphics.Blit(rt1, rt2, material, 0);
+            Graphics.Blit(rt2, rt1, material, 1);
+            Graphics.Blit(rt1, destination);
+            //RenderTexture.ReleaseTemporary(rt);
+            RenderTexture.ReleaseTemporary(rt1);
+            RenderTexture.ReleaseTemporary(rt2);
         }
 
         private float[] GetKernel(int radius) {
@@ -47,20 +61,23 @@ namespace UnityStandardAssets.ImageEffects {
             var result = new float[MaxRadius + 1];
             var sum = 0.0f;
             for (int i = 0; i < radius + 1; i++) {
-                result[i] = CalculateKernelElement(i, 0, radius + 1);
+                result[i] = CalculateKernelElement(i, 0, radius);
                 for (int j = 1; j < radius + 1; j++) {
-                    result[i] += CalculateKernelElement(i, j, radius + 1) * 2;
+                    result[i] += CalculateKernelElement(i, j, radius);
                 }
                 sum += result[i] * ((i == 0) ? 1 : 2);
             }
             for (int i = 0; i < radius + 1; i++) {
                 result[i] = result[i] / sum;
             }
+            //Debug.Log(radius);
+            //Debug.Log(String.Join(" ; ", result.Select(_ => _.ToString()).ToArray())); 
             return result;
         }
 
-        private float CalculateKernelElement(int x, int y, float sigma) {
-            return 1/(2 * Mathf.PI * sigma * sigma) * Mathf.Exp(-(x * x + y * y) / (2 * sigma * sigma));
+        private float CalculateKernelElement(int x, int y, int radius) {
+            float sigma = (float)radius / 3;
+            return 1 / (2 * Mathf.PI * sigma * sigma) * Mathf.Exp(-(x * x + y * y) / (2 * sigma * sigma));
         }
     }
 }
