bc378930292faf3598f74a8fa5a18b1bff6033a8
Andrey Chelushkin <achelushkin@andiks.com>
2016-10-26 19:45:06 +0300

    Blur update

---
+++ Assets/Materials/Shaders/PostProcess/GaussianBlur.shader
@@ -0,0 +1,104 @@
+﻿Shader "Hidden/Blur/GaussianBlur"
+{
+	Properties
+	{
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+
+			int _Radius;
+			float _Kernel[32];
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv) * _Kernel[0];
+				for (int p = 1; p < _Radius + 1; p++) {
+					col += tex2D(_MainTex, i.uv + float2(0, p * _MainTex_TexelSize.y)) * _Kernel[p];
+					col += tex2D(_MainTex, i.uv - float2(0, p * _MainTex_TexelSize.y)) * _Kernel[p];
+				}
+				return col;
+			}
+			ENDCG
+			}
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+
+				#include "UnityCG.cginc"
+
+				sampler2D _MainTex;
+				float4 _MainTex_TexelSize;
+
+				int _Radius;
+				float _Kernel[32];
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					float4 vertex : SV_POSITION;
+				};
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+					o.uv = v.uv;
+					return o;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					fixed4 col = tex2D(_MainTex, i.uv) * _Kernel[0];
+					for (int p = 1; p < _Radius + 1; p++) {
+						col += tex2D(_MainTex, i.uv + float2(p * _MainTex_TexelSize.x, 0)) * _Kernel[p];
+						col += tex2D(_MainTex, i.uv - float2(p * _MainTex_TexelSize.x, 0)) * _Kernel[p];
+					}
+					return col;
+				}
+				ENDCG
+				}
+	}
+}
--- Assets/Scripts/Visual/PostProcessing/BlurPostProcess.cs
+++ Assets/Scripts/Visual/PostProcessing/BlurPostProcess.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UnityStandardAssets.ImageEffects {
@@ -7,8 +9,12 @@ namespace UnityStandardAssets.ImageEffects {
 
         [SerializeField]
         private Shader _MobileShader;
-        [Range(0.1f, 1)]
+        [Range(0, 1)]
         public float Intensity;
+
+        private const int MaxRadius = 32;
+
+        private Dictionary<int, float[]> _KernelCache = new Dictionary<int, float[]>();
 
         public override Shader Shader {
             get {
@@ -21,13 +27,40 @@ namespace UnityStandardAssets.ImageEffects {
         }
 
         void OnRenderImage(RenderTexture source, RenderTexture destination) {
-            var downsample = Intensity * Intensity;
-            var rt = RenderTexture.GetTemporary((int)(source.width * downsample), (int)(source.height * downsample), 0, source.format);
-            rt.filterMode = FilterMode.Bilinear;
-            Graphics.Blit(source, rt);
-            Graphics.Blit(rt, destination);
+            var radius = (int)(MaxRadius * Intensity);
+            var rt = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            material.SetInt("_Radius", radius);
+            material.SetFloatArray("_Kernel", GetKernel(radius));
+            Graphics.Blit(source, rt, material, 0);
+            Graphics.Blit(rt, destination, material, 1);
             RenderTexture.ReleaseTemporary(rt);
-            //Graphics.Blit(source, destination, material);
+        }
+
+        private float[] GetKernel(int radius) {
+            if (!_KernelCache.ContainsKey(radius)) {
+                _KernelCache.Add(radius, GenerateKernel(radius));
+            }
+            return _KernelCache[radius];
+        }
+
+        private float[] GenerateKernel(int radius) {
+            var result = new float[MaxRadius + 1];
+            var sum = 0.0f;
+            for (int i = 0; i < radius + 1; i++) {
+                result[i] = CalculateKernelElement(i, 0, radius + 1);
+                for (int j = 1; j < radius + 1; j++) {
+                    result[i] += CalculateKernelElement(i, j, radius + 1) * 2;
+                }
+                sum += result[i] * ((i == 0) ? 1 : 2);
+            }
+            for (int i = 0; i < radius + 1; i++) {
+                result[i] = result[i] / sum;
+            }
+            return result;
+        }
+
+        private float CalculateKernelElement(int x, int y, float sigma) {
+            return 1/(2 * Mathf.PI * sigma * sigma) * Mathf.Exp(-(x * x + y * y) / (2 * sigma * sigma));
         }
     }
 }
